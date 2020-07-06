163bd5c890b7bc55e4e7c7d1b5ac247571fb5aec
claire-ca <57175630+claire-ca@users.noreply.github.com>
2020-07-06 16:01:40 +0100

    Content Filtering added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-;( function( $ ) {
+( function( $ ) {
 
 	$( '.swipebox' ).swipebox({
 		useCSS : true, // false will force the use of jQuery for animations
@@ -7,7 +7,7 @@
 		initialIndexOnArray : 0, // which image index to init when a array is passed
 		hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
 		removeBarsOnMobile : false, // false will show top bar on mobile devices
-		hideBarsDelay : 6000, // delay before hiding bars on desktop
+		hideBarsDelay : 4000, // delay before hiding bars on desktop
 		beforeOpen: function() {}, // called before opening
 		afterOpen: null, // called after opening
 		afterClose: function() {}, // called after closing
@@ -15,3 +15,13 @@
 	});
 
 } )( jQuery );
+
+$("#gallery-search").keyup(function() {
+    var val = $.trim(this.value.toLowerCase()); // gets the value in the input field. Trims whitespace from beginning and end of input. I added toLowerCase().
+    if (val === "") // if val is empty 
+        $('a').show(); // show all the a tags
+    else { // otherwise
+        $('a').hide(); // hide all the a tags
+        $('a[title*=' + val + "]").show(); // unless the title attribute contains the characters written in the input field
+    }
+});
